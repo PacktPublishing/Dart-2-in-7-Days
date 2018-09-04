64029abed4a1ed1c3d0de7ebd4f1b430cdc5dccb
skooltch84 <skooltch84@users.noreply.github.com>
2018-09-04 18:07:46 -0300

    Add files via upload
    Added Section 2.1

---
+++ 02 - Variables, Data Types and Control Flow/2.1 - Dart - Introduction to Variables and Data Types/Playground/bin/main.dart
@@ -0,0 +1,34 @@
+
+
+void main() {
+ //print("hello world");
+ //Creating Variables
+ var firstName = "Victor";
+ var age = 33;
+
+ //firstName = 33;
+ //age = "Victor";
+
+ firstName = "Steve";
+ age = 26;
+
+ String lastName = 'Wooding';
+ int currentAge = 33;
+
+ double height = 181.61;
+ num milesToBridgetown = 17;
+
+ bool isSunny = true;
+ bool isHungry = false;
+
+ print(firstName);
+ print(age);
+ print(lastName);
+ print(currentAge);
+ print(height);
+ print(milesToBridgetown);
+ print(isSunny);
+ print(isHungry);
+
+
+}
