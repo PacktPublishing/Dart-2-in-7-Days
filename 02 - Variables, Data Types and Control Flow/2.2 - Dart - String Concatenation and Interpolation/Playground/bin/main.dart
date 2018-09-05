edc53b2779cd8d5d0f1f161a933b534c1be282e1
skooltch84 <skooltch84@users.noreply.github.com>
2018-09-05 18:14:56 -0300

    Added String Concatenation and Interpolation

---
+++ 02 - Variables, Data Types and Control Flow/2.2 - Dart - String Concatenation and Interpolation/Playground/bin/main.dart
@@ -0,0 +1,59 @@
+
+
+void main() {
+ /*
+ //print("hello world");
+ //Creating Variables
+ var firstName = "Victor";
+ var age = 33;
+
+ //firstName = 33;
+ //age = "Victor";
+
+ firstName = "Steve";
+ age = 26;
+
+ String lastName = 'Wooding';
+ int currentAge = 33;
+
+ double height = 181.61;
+ num milesToBridgetown = 17;
+
+ bool isSunny = true;
+ bool isHungry = false;
+
+ print(firstName);
+ print(age);
+ print(lastName);
+ print(currentAge);
+ print(height);
+ print(milesToBridgetown);
+ print(isSunny);
+ print(isHungry);
+
+*/
+
+//String Concatenation
+
+ String name = "Victor";
+ String greeting = "Hello";
+
+ String fullGreeting = greeting + " " + name;
+ print(fullGreeting);
+
+ //String Interpolation
+
+ name = "Steve";
+ var groceries = "milk, cheese, eggs, ham and bread";
+ name = "Michael";
+  var someString = "Today $name is going to the store to buy $groceries!";
+
+  print(someString);
+
+  print(name.toUpperCase());
+  print(name);
+
+  print(name.toLowerCase());
+  print(name);
+
+}
