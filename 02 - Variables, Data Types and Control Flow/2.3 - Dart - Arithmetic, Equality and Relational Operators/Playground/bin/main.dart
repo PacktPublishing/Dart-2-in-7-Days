fde073e8605d6eae187702664ea71e62eaf02486
skooltch84 <skooltch84@users.noreply.github.com>
2018-09-06 21:45:20 -0300

    Arithmetic, Equality and Relational Operators

---
+++ 02 - Variables, Data Types and Control Flow/2.3 - Dart - Arithmetic, Equality and Relational Operators/Playground/bin/main.dart
@@ -0,0 +1,87 @@
+
+
+void main() {
+ /*
+ //print("hello world");
+ //Creating Variables
+ var firstName = "Victor";
+ var age = 33;
+
+ //firstName = 33;
+ //age = "Victor";
+
+ firstName = "Steve";
+ age = 26;
+
+ String lastName = 'Wooding';
+ int currentAge = 33;
+
+ double height = 181.61;
+ num milesToBridgetown = 17;
+
+ bool isSunny = true;
+ bool isHungry = false;
+
+ print(firstName);
+ print(age);
+ print(lastName);
+ print(currentAge);
+ print(height);
+ print(milesToBridgetown);
+ print(isSunny);
+ print(isHungry);
+
+*/
+
+//String Concatenation
+/*
+ String name = "Victor";
+ String greeting = "Hello";
+
+ String fullGreeting = greeting + " " + name;
+ print(fullGreeting);
+
+ //String Interpolation
+
+ name = "Steve";
+ var groceries = "milk, cheese, eggs, ham and bread";
+ name = "Michael";
+  var someString = "Today $name is going to the store to buy $groceries!";
+
+  print(someString);
+
+  print(name.toUpperCase());
+  print(name);
+
+  print(name.toLowerCase());
+  print(name);
+*/
+
+//Operators
+  var a = 2;
+  var b = 3;
+
+  //additive (+ -)
+  print(a+b);
+
+  a = a + 2;
+  print(a);
+  print(a-b);
+
+
+  var c = 4;
+  var d = 5;
+
+  //multiplicative (* /)
+  print("The product of $c and $d is ${c * d}");
+  print("$c divided by $d is ${c / d}");
+  print("The remainder when $d is divided by $c is ${d % c}");
+
+  //equality (== !=)
+  print(c == d);
+  print(c != d);
+
+  //relational (<=  <  >  >=)
+  print (4 > 5);
+  print (4 < 5);
+}
