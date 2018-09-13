11f68d1f119dbf0403e5bfcec1ed826f086fd3fc
skooltch84 <skooltch84@users.noreply.github.com>
2018-09-12 21:30:48 -0300

    Code Challenge

---
+++ 02 - Variables, Data Types and Control Flow/Code Challenge/section_2_code_challenge/bin/main.dart
@@ -0,0 +1,23 @@
+
+void main() {
+
+//create the for loop first
+//then create an if statement to perform a check
+//nest if statement in the for loop
+
+for (var i = 0; i < 25; i++){
+  if((i * 4) % 8 == 0){
+    print("4 x $i is ${i * 4}");
+  }
+
+  
+}
+
+
+
+
+
+
+
+
+}
---
+++ 02 - Variables, Data Types and Control Flow/Code Challenge/section_2_code_challenge/test/section_2_code_challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:section_2_code_challenge/section_2_code_challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
